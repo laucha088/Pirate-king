e8f0824b0ba9717215125f2350dc006cdfee7d2b
laucha088 <laubazan0@gmail.com>
2023-05-31 18:08:51 -0300

    Intento de tarea con loader
    no me compila ,

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1000,
+	height: 650
 });
 
-//const clampy: Sprite = Sprite.from("clampy.png");
-const Perritococa: Sprite = Sprite.from("perritococa");
-Perritococa.anchor.set(200);
-//clampy.anchor.set(0.5);
-Perritococa.x = app.screen.width /2;
-Perritococa.y = app.screen.height /2;
-//clampy.x = app.screen.width / 2;
-//clampy.y = app.screen.height / 2;
-app.stage.addChild(Perritococa)
-//app.stage.addChild(clampy);
+Loader.shared.add({url:"./Perrito.jpg",name: "Perrito"});
+Loader.shared.add({url:"./clampy.png",name: "Clampy"});
+
+Loader.shared.onComplete.add(()=>{
+	const clampy: Sprite = Sprite.from("Perrito");
+
+	console.log(clampy.width,clampy.height);
+
+	clampy.anchor.set(0.6);
+	
+	clampy.x = app.screen.width / 1.5;
+	clampy.y = app.screen.height / 1.8;
+
+	app.stage.addChild(clampy);
+
+});
+Loader.shared.Load();
